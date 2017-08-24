25e2a893137572146e696064b05a936495a55611
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2017-08-24 21:39:11 +0200

    lm example type error fix

--- examples/lm-simple/LM.hs
+++ examples/lm-simple/LM.hs
@@ -11,8 +11,8 @@ module Aggr where
 
 import TypedFlow
 
-lm :: forall (vocSize::Nat) batchSize. KnownNat vocSize => KnownNat batchSize => Model '[21,batchSize] Int32 '[batchSize] Int32
-lm input gold = do
+predict :: forall (vocSize::Nat) batchSize. KnownNat vocSize => KnownNat batchSize => Model '[21,batchSize] Int32 '[batchSize] Int32
+predict input gold = do
   embs <- parameter "embs" embeddingInitializer
   lstm1 <- parameter "w1" lstmInitializer
   let drp = dropout (KeepProb 0.9)
@@ -22,16 +22,14 @@ lm input gold = do
           .-.
           timeDistribute drp
           .-.
-          (onState drp (lstm @50 lstm1))
-          .-.
-          timeDistribute (softmax0 . squeeze0 . dense  w))
+          (onState drp (lstm @50 lstm1)))
         (I (zeros,zeros) :* Unit) input
-  categorical (last0 predictions) gold
+  categorical ((dense @vocSize w) (last0 predictions)) gold
 
 
 main :: IO ()
 main = do
-  generateFile "lm.py" (compile (defaultOptions {maxGradientNorm = Just 1}) (lm @8 @128))
+  generateFile "lm.py" (compile (defaultOptions {maxGradientNorm = Just 1}) (predict @8 @128))
   putStrLn "done!"
 
 (|>) :: ∀ a b. a -> b -> (a, b)
@@ -41,6 +39,18 @@ infixr |>
 
 {-> main
 
+Parameters (total 12480):
+dense_snd: T [8] tf.float32
+dense_fst: T [8,50] tf.float32
+w1_4_snd: T [50] tf.float32
+w1_4_fst: T [50,59] tf.float32
+w1_3_snd: T [50] tf.float32
+w1_3_fst: T [50,59] tf.float32
+w1_2_snd: T [50] tf.float32
+w1_2_fst: T [50,59] tf.float32
+w1_1_snd: T [50] tf.float32
+w1_1_fst: T [50,59] tf.float32
+embs: T [9,8] tf.float32
 done!
 -}
 
