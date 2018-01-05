2c4ab1b21864f505cc3cd3e219275d54608a7436
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2018-01-05 11:00:59 +0100

    complete tdlm

--- TypedFlow/Models/Topic.hs
+++ TypedFlow/Models/Topic.hs
@@ -35,20 +35,9 @@ import Prelude hiding ((/), sqrt)
 import TypedFlow.TF
 import TypedFlow.Layers
 import TypedFlow.Types
+import TypedFlow.Learn
 import GHC.TypeLits
 
--- -- | create a document summarization function with appropriate parameters.
--- mkDocumentSummary
---   :: String -> -- ^ prefix for parameter names
---      Gen (T '[n,e] (Flt t) -> T '[a] (Flt t)) -- ^ document vector (summary)
--- mkDocumentSummary prefix = do
---   filter <- parameter (prefix ++ "_filter") (truncatedNormal 0.1 )
---   return $ (relu . conv filter)
-
-
-
--- p = softmax (A d)
--- s = B p
 
 -- | A convolutional document summary function. Described in
 -- 'Topically Driven Neural Language Model' by Lau, Baldwin and Cohn.
@@ -67,27 +56,35 @@ tldmDocsummary embs filters dropProb document = do
   return (reduceMax @Dim1 (conv filters (drpEmb (embedding @e @vocSize embs document))))
 
 -- | A topic modeler. Described 'Topically Driven Neural Language
--- Model' by Lau, Baldwin and Cohn.
-tdlmTopic :: forall
+-- Model' by Lau, Baldwin and Cohn.  Returns a function converting raw
+-- representations (eg. document summaries) to topic representations.
+-- This representation can be used as input to a dense layer to
+-- predict a word, or as input to an LSTM (initial state) to predict
+-- sentences.
+mkTdlmTopic :: forall
   (kk :: Nat) -- number of topics
   (a :: Nat) -- document vector summary size
   (b :: Nat) -- topic representation size
   (t :: NBits) -- size of floats
   (batchSize :: Nat)
   . KnownNat kk => KnownNat a => KnownNat b => KnownBits t => KnownNat batchSize
-  => T '[a,batchSize] (Flt t) -- ^ document summary
-  -> Gen (Tensor '[b, batchSize] (Flt t), Scalar (Flt t))
-tdlmTopic d = do
+  => Gen (T '[a,batchSize] (Flt t) -> Tensor '[b, batchSize] (Flt t))
+mkTdlmTopic = do
   drpS   <- mkDropout (DropProb 0.1)
   topicInput :: T '[a,kk] (Flt t) <- parameter "A" glorotUniform -- mapping from document representations to topics
   topicOutput :: T '[kk,b] (Flt t) <- parameter "B" glorotUniform  -- all possible topics
-  let p :: T '[kk,batchSize] (Flt t)
-      p = softmax0 (topicInput ∙ d) -- attention distribution (among the topics)
-      s :: T '[b,batchSize] (Flt t)
-      s = drpS (topicOutput ∙ p)  -- document topic representation
+  -- regularizer which ensures that topics are disjoint:
+  let
       topicNormalized :: T '[b,kk] (Flt t)
-      topicNormalized = transpose01 topicOutput / (sqrt (reduceSum @Dim0 (topicOutput ⊙ topicOutput)) :: T '[b] (Flt t))
+      topicNormalized = transpose01 topicOutput / (sqrt (reduceSum @Dim0 (square topicOutput)) :: T '[b] (Flt t))
+      -- matrix of correlation between the topics
       topicCorrelation :: T '[b,b] (Flt t)
       topicCorrelation = matmul (transpose01 topicNormalized) topicNormalized
-      topicUniqueness = reduceMaxAll (topicCorrelation ⊝ eye)
-  return (s,topicUniqueness)
+      -- max correlation between two distinct topics
+      topicOverlap = reduceMaxAll (square (topicCorrelation ⊝ eye))
+  addRegularizer (cast topicOverlap)
+  
+  return (\d -> let p :: T '[kk,batchSize] (Flt t)
+                    p = softmax0 (topicInput ∙ d) -- attention distribution (among the topics)
+                in drpS (topicOutput ∙ p))
+
