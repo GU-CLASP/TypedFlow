9122d627b86f334c030a05620542d681c0c73a18
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2021-09-02 14:20:44 +0200

    python placeholders: advertise type

--- TypedFlow/Python.hs
+++ TypedFlow/Python.hs
@@ -467,7 +467,11 @@ compileAlreadyBatched Options{..} model = knownAppend @sy @ps ?> do
     gen (text "return " <> dict [("batch_size", (showDim @ bs))
                                 ,("parameters",list (map pyVarInfoRepr parameters))
                                 ,("paramsdict",dict [(varName, pyVarInfoRepr vi) | vi@VarInfo {..} <- parameters])
-                                ,("placeholders",dict [(varName, dict [("shape",case varRef of Ref _id s _t -> showSShape s)]) | VarInfo {..} <- placeHolders] )
+                                ,("placeholders",dict [(varName,
+                                                         case varRef of Ref _id shape typ ->
+                                                                          dict [("shape",showSShape shape),
+                                                                                 ("dtype",showSTyp typ)])
+                                                      | VarInfo {..} <- placeHolders])
                                 ])
 
   modify $ \PyState {..} -> PyState {genAssignTable = M.empty,..} -- we can't re-use intermediate computations from initialisers
